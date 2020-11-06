dbd7945ab442ae9069542fe0a424f2556ebc12d1
betsystevens <betsystevens5@gmail.com>
2020-11-06 13:48:19 -0600

    refactor makePoint return object vs array, rearrange code

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 const fieldSize = 5;
 const BOMB_ENUM = 7;
 
+/*********** */
+/* dom field */
+/*********** */
+
 /* add rows and cells(bricks) to the dom table */
 const makeTable = function(size, tableID) {
   let table = document.getElementById(tableID);
@@ -11,51 +15,15 @@ const makeTable = function(size, tableID) {
   }
   return table;
 }
-
 // add cells(bricks)to a row
 const insertBricks = function(brickCount){
   for (let i=0; i<brickCount; i++){
     this.insertCell().className = "brick";
   }
 }
-/* create a random point: [x,y] */
-const randomPoint = function(max) {
-  return function() {
-    let x = Math.floor(Math.random() * Math.floor(max));
-    let y = Math.floor(Math.random() * Math.floor(max));
-    return [x,y]; 
-  }
-}
-const getPoint = randomPoint(fieldSize);
-
-/* create a virtual field  */
-const virtualField = function(size) {
-  let field = [];
-  for (let i = 0; i < size; i++){
-    field.push([]);
-  }
-  return {
-    size, 
-    field,
-    getField() {
-      return this.field;
-    },
-    /* randomly place bombs */
-    placeBombs(bombCount){
-      for (let i=0; i < bombCount; i++) {
-        let p = getPoint();
-        this.field[p[0]][p[1]] = BOMB_ENUM;
-      }
-      return this;
-    }
-  }
-}
-
-const field = virtualField(fieldSize).placeBombs(15).getField();
-console.log(field);
+const table = makeTable(fieldSize, "grid");
 
 /* add event listeners and handlers */
-const table = makeTable(fieldSize, "grid");
 table.addEventListener("click", handleClick, false);
 table.addEventListener("contextmenu", handleRightClick, false);
 
@@ -72,4 +40,44 @@ function handleRightClick(e) {
   e.preventDefault();
   e.target.className = "brick flag";
 }
+
+/*************** */
+/* virtual field */
+/*************** */
+
+/* create a random point: [x,y] */
+const randomPoint = function(max) {
+  return function() {
+    let x = Math.floor(Math.random() * Math.floor(max));
+    let y = Math.floor(Math.random() * Math.floor(max));
+    return { x, y };
+  }
+}
+/* create a virtual field  */
+const virtualField = function(size) {
+  let field = [];
+  for (let i = 0; i < size; i++){
+    field.push([]);
+  }
+  const getPoint = randomPoint(fieldSize);
+  return {
+    size, 
+    field,
+    getField() {
+      return this.field;
+    },
+    /* randomly place bombs */
+    placeBombs(bombCount){
+      for (let i=0; i < bombCount; i++) {
+        let p = getPoint();
+        this.field[p.x][p.y] = BOMB_ENUM;
+      }
+      return this;
+    }
+  }
+}
+
+const field = virtualField(fieldSize).placeBombs(15).getField();
+console.log(field);
+
 
